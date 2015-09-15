753f96f8b09d65ea5ab0e47c6decb7afb495b3e3
Tim Hyson <tjhyson@gmail.com>
2015-09-15 12:32:56 +0100

    Added new_game page, RSpec pass

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -2,9 +2,15 @@ require 'sinatra/base'
 
 class BattleshipsWeb < Sinatra::Base
   get '/' do
-    'Hello BattleshipsWeb!'
+    erb :index
+  end
+
+  get '/new_game' do
+    erb :new_game
   end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
+
+  set :views, proc { File.join(root, '..', 'views') }
 end
